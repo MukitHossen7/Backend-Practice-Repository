0e5a4973441e14667521518e40476f7ad326479b
MukitHossen7 <hossenmukit7@gmail.com>
2024-11-25 00:21:51 +0600

    succesful data load

---
+++ Module-55/simple-crud-oprations/src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+
+function App() {
+  const handleFrom = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  return (
+    <div>
+      <h2>Simple CURD</h2>
+      <form onSubmit={handleFrom}>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
