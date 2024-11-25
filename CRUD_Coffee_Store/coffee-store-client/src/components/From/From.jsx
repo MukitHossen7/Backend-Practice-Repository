cd311508814b115a4ebe5c53828cf91bd3aeb437
MukitHossen7 <hossenmukit7@gmail.com>
2024-11-26 00:42:11 +0600

    passing data database

--- CRUD_Coffee_Store/coffee-store-client/src/components/From/From.jsx
+++ CRUD_Coffee_Store/coffee-store-client/src/components/From/From.jsx
@@ -1,9 +1,48 @@
+import swal from "sweetalert";
+
 const From = () => {
+  const handleCoffeeForm = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const supplier = e.target.supplier.value;
+    const category = e.target.category.value;
+    const chef = e.target.chef.value;
+    const taste = e.target.taste.value;
+    const details = e.target.details.value;
+    const photo = e.target.photo.value;
+
+    const coffeesData = {
+      name,
+      supplier,
+      category,
+      chef,
+      taste,
+      details,
+      photo,
+    };
+    console.log(coffeesData);
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(coffeesData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId.length > 0) {
+          e.target.reset();
+          swal("success", "Successfully added coffees data", "success");
+        }
+      });
+  };
+
   return (
     <div className="pt-10">
       <h2 className="font-medium text-4xl text-center mb-10">Add New Coffee</h2>
       <div>
-        <form>
+        <form onSubmit={handleCoffeeForm}>
           <div className="flex gap-10">
             <div className="w-full lg:w-1/2 flex flex-col gap-4">
               <div>
@@ -13,6 +52,8 @@ const From = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  name="name"
+                  required
                   placeholder="Enter coffee name"
                 />
               </div>
@@ -23,6 +64,8 @@ const From = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  name="supplier"
+                  required
                   placeholder="Enter coffee supplier"
                 />
               </div>
@@ -33,6 +76,8 @@ const From = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  name="category"
+                  required
                   placeholder="Enter coffee category"
                 />
               </div>
@@ -45,6 +90,8 @@ const From = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  name="chef"
+                  required
                   placeholder="Enter coffee chef"
                 />
               </div>
@@ -55,6 +102,8 @@ const From = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  name="taste"
+                  required
                   placeholder="Enter coffee taste"
                 />
               </div>
@@ -65,6 +114,8 @@ const From = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
+                  name="details"
+                  required
                   placeholder="Enter coffee details"
                 />
               </div>
@@ -77,16 +128,16 @@ const From = () => {
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
+              name="photo"
+              required
               placeholder="Enter photo URL"
             />
           </div>
-
-          <button
-            type="button"
-            className="btn bg-green-600 hover:bg-green-600 text-white w-full mt-7"
-          >
-            Add Coffee
-          </button>
+          <input
+            type="submit"
+            value="Add Coffee"
+            className="btn bg-green-600 hover:bg-green-500 text-white w-full mt-7"
+          />
         </form>
       </div>
     </div>
