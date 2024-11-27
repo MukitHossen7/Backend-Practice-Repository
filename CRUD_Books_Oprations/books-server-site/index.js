c3996fe4521231504a6f16cde25d84a78bdae678
MukitHossen7 <hossenmukit7@gmail.com>
2024-11-27 19:56:52 +0600

    curd opration is successful

--- CRUD_Books_Oprations/books-server-site/index.js
+++ CRUD_Books_Oprations/books-server-site/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -31,13 +31,39 @@ async function run() {
       const result = await bookCollection.find().sort({ _id: -1 }).toArray();
       res.send(result);
     });
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.findOne(query);
+      res.send(result);
+    });
     app.post("/books", async (req, res) => {
       const book = req.body;
-      console.log(book);
       const result = await bookCollection.insertOne(book);
       res.send(result);
     });
-
+    app.delete("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.put("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const book = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateBook = {
+        $set: {
+          name: book.name,
+          author: book.author,
+          length: book.length,
+          photo: book.photo,
+        },
+      };
+      const result = await bookCollection.updateOne(query, updateBook, options);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
