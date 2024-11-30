57bb512e00834fe619c026cea9443bebc99cba21
MukitHossen7 <hossenmukit7@gmail.com>
2024-11-30 16:25:02 +0600

    some problem slove

--- CRUD_Coffee_Store/coffee-store-server/index.js
+++ CRUD_Coffee_Store/coffee-store-server/index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("coffeeDB");
     const coffeeCollection = database.collection("coffee");
@@ -103,7 +103,7 @@ async function run() {
       const result = await usersCollection.updateOne(query, updatedUser);
       res.send(result);
     });
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
