951f9cf829bbfd73f5618414abdc26e03f827902
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-02 11:33:06 +0600

    All schedule page design is done

--- GYM_Schedule_CRUD/gym-client/src/Pages/AllScedule/AllScedule.jsx
+++ GYM_Schedule_CRUD/gym-client/src/Pages/AllScedule/AllScedule.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const AllScedule = () => {
+  const [search, setSearch] = useState();
+  console.log(search);
   return (
-    <div>
-      <h2>All Schedule</h2>
+    <div className="mt-10">
+      <div className="w-[400px] mx-auto mb-4">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          name="search"
+          placeholder="search"
+          className="input input-bordered w-full"
+          required
+        />
+      </div>
+      <div className="w-11/12 mx-auto bg-slate-50">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>serial</th>
+                <th>Title</th>
+                <th>Day</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Auction</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
