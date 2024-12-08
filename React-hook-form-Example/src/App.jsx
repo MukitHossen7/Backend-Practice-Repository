20b151710439f87c5911839c8498b00ae61b6985
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-08 23:49:47 +0600

    react hock  form useges

--- React-hook-form-Example/src/App.jsx
+++ React-hook-form-Example/src/App.jsx
@@ -1,29 +1,60 @@
+import { useForm } from "react-hook-form";
 const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleLogin = (data) => {
+    const { email, password } = data;
+    console.log(email, password);
+  };
   return (
     <div className="w-11/12 mx-auto h-screen mt-10">
       <h2 className="text-3xl text-center">Login Form</h2>
-      <form className="card-body max-w-xl shadow-lg mx-auto">
+      <form
+        className="card-body max-w-xl shadow-lg mx-auto"
+        onSubmit={handleSubmit(handleLogin)}
+      >
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
           <input
-            type="email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+            })}
             placeholder="email"
             className="input input-bordered"
-            required
           />
+          {
+            <span className="text-red-500 text-xs font-semibold mt-1">
+              {errors.email?.message}
+            </span>
+          }
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
-            type="password"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "Password is required",
+              },
+            })}
             placeholder="password"
             className="input input-bordered"
-            required
           />
+          {
+            <span className="text-red-500 text-xs font-semibold mt-1">
+              {errors.password?.message}
+            </span>
+          }
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">
               Forgot password?
