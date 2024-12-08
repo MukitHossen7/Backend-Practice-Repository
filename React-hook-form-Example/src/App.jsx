b9526d882ad300ee9bbbab0be8d75fcded0383d2
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-08 21:14:42 +0600

    react hock form desing and file ready

---
+++ React-hook-form-Example/src/App.jsx
@@ -0,0 +1,41 @@
+const App = () => {
+  return (
+    <div className="w-11/12 mx-auto h-screen mt-10">
+      <h2 className="text-3xl text-center">Login Form</h2>
+      <form className="card-body max-w-xl shadow-lg mx-auto">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default App;
